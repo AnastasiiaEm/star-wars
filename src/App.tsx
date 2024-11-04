b995aec01fc9f5a51d1f75449c9982ab3b316cf2
anastasiia-em-i <167425361+anastasiia-em-i@users.noreply.github.com>
2024-11-04 16:33:44 +0100

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { Home } from './pages/Home/Home';
 import { OpeningCrawl } from './pages/OpeningCrawl/OpeningCrawl';
 import { PersonDetails } from './pages/PersonDetails/PersonDetails';
 
 const queryClient = new QueryClient();
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <OpeningCrawl />,
