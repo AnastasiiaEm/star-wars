cf9e3d6a5fc7bcf18cd98cea7a501c0bcf1a4e3b
anastasiia-em-i <167425361+anastasiia-em-i@users.noreply.github.com>
2024-11-04 16:49:19 +0100

    Changed logo url

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = () => {
   return (
     <Link  to="/characters" className="logo">
       <img
-        src={'/assets/star-wars-4.svg'}
+        src={'./assets/star-wars-4.svg'}
         alt="LOGO"
         className="logo__img"
       />
